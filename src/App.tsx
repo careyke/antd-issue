381151b22d1223a36418c00c5e02d838e456b2a7
hermanke <hermanke@tencent.com>
2021-06-16 19:52:44 +0800

    feat: react-hot-loader

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,15 @@
 import React, { FC } from "react";
-import { Breadcrumb } from "antd";
+import { Button } from "antd";
+import { hot } from "react-hot-loader";
 
 import classes from "./App.less";
 
 const App: FC = () => {
   return (
     <div className={classes.container}>
-      <Breadcrumb>
-        <Breadcrumb.Item>1</Breadcrumb.Item>
-        <Breadcrumb.Item>2</Breadcrumb.Item>
-      </Breadcrumb>
+      <Button type="primary">click</Button>
     </div>
   );
 };
 
-export default App;
+export default hot(module)(App);
--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,10 @@ import ReactDOM from "react-dom";
 
 import App from "./App";
 
+// if ((module as CommonObject).hot) {
+//   (module as CommonObject).hot.accept();
+// }
+
 const root: Element | null = document.querySelector("#app");
 
 ReactDOM.render(<App />, root);
